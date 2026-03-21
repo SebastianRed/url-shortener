992b279b0119e58ff3bbbc4c6c6930d04c59a0fc
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-03-20 22:20:56 -0300

    Agrega campo de fecha de creación y ajusta longitud de short_url en la entidad Url

--- src/main/java/cl/sebastianrojo/url_shortener/Entity/Url.java
+++ src/main/java/cl/sebastianrojo/url_shortener/Entity/Url.java
@@ -1,5 +1,9 @@
 package cl.sebastianrojo.url_shortener.Entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,19 +18,22 @@ public class Url {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+ 
     @Column(name = "original_url", nullable = false, length = 2000)
     private String originalUrl;
-
-    @Column(name = "short_url", nullable = false, unique = true)
+ 
+    @Column(name = "short_url", nullable = false, unique = true, length = 6)
     private String shortUrl;
-
+ 
     @Column(name = "access_count", nullable = false)
     private Long accessCount = 0L;
-
-    public Url() {
-    }
-
+ 
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+ 
+    public Url() {}
+ 
     public Url(String originalUrl, String shortUrl) {
         this.originalUrl = originalUrl;
         this.shortUrl = shortUrl;
@@ -64,4 +71,11 @@ public class Url {
         this.accessCount = accessCount;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
